f960261af28364036e66589d1ddf72a0390213dc
cheyenne <cheyenne.nguyen@bisonok.com>
2021-09-28 14:07:54 -0500

    My very first project.

---
+++ script.js
@@ -0,0 +1,14 @@
+const count = document.querySelector(".count");
+const add = document.querySelector(".add");
+const resetCount = document.querySelector(".reset");
+const sub = document.querySelector(".subtract");
+
+add.addEventListener("click" , () => {
+    count.innerHTML++;
+})
+add.addEventListener("click" , () => {
+    count.innerHTML++;
+})
+add.addEventListener("click" , () => {
+    count.innerHTML++;
+})
